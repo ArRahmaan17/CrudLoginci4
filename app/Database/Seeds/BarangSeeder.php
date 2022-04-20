4df736e50ef5fcf78aff1c0d5802d4b5f96bec5d
ArRahmaan17 <ardrah17@gmail.com>
2022-04-20 10:07:49 +0700

    Update View

---
+++ app/Database/Seeds/BarangSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class BarangSeeder extends Seeder
+{
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+        for ($i=0; $i < 50; $i++) { 
+            $data = [
+                'nama' => $faker->name($gender = 'male'|'female'),
+                'jumlah' => $faker->randomNumber(4, true),
+                'hargasatuan' => $faker->numerify('Rp.#####'),
+                'dimensi' => $faker->randomElement(['Lt','Kg','Pcs']),
+                'tanggalmasuk' => $faker->date(),
+            ];
+            // $this->db->query("INSERT INTO pesanan (nama, barang, jumlah, tanggalmasuk, status) VALUES(:nama:, :barang:, :jumlah:, :tanggalmasuk:, :status:)", $data);
+            // Using Query Builder
+            $this->db->table('barangmasuk')->insert($data);
+        }
+    }
+}
